85b598c0579e92b2389589aaf8408124d2317246
Mike Erickson <codedungeon@gmail.com>
2019-03-24 20:26:37 -0700

    chore: fix testing check

--- src/Messenger.php
+++ src/Messenger.php
@@ -68,7 +68,7 @@ class Messenger
      */
     public function packageName()
     {
-        $content = file_get_contents($this->getPackageRoot() . DIRECTORY_SEPARATOR . 'composer.json');
+        $content = file_get_contents($this->getPackageRoot().DIRECTORY_SEPARATOR.'composer.json');
         if ($content) {
             $content = json_decode($content, true);
 
@@ -87,19 +87,19 @@ class Messenger
     }
 
     /**
-     * @param string $configFileName
+     * @param  string  $configFileName
      * @return string
      */
     public function getConfigurationFile($configFileName = 'php-messenger.yml')
     {
-        $defaultConfigFilename = $this->getPackageRoot() . DIRECTORY_SEPARATOR . 'src/' . $configFileName;
+        $defaultConfigFilename = $this->getPackageRoot().DIRECTORY_SEPARATOR.'src/'.$configFileName;
 
         $configPath = getcwd();
         $filename = '';
 
         $continue = true;
         while (!file_exists($filename) && $continue) {
-            $filename = $configPath . DIRECTORY_SEPARATOR . $configFileName;
+            $filename = $configPath.DIRECTORY_SEPARATOR.$configFileName;
             if (($this->isWindows() && \strlen($configPath) === 3) || $configPath === '/') {
                 $filename = $defaultConfigFilename;
                 $continue = false;
@@ -123,7 +123,7 @@ class Messenger
      */
     public function version()
     {
-        $content = file_get_contents($this->getPackageRoot() . DIRECTORY_SEPARATOR . 'composer.json');
+        $content = file_get_contents($this->getPackageRoot().DIRECTORY_SEPARATOR.'composer.json');
         if ($content) {
             $content = json_decode($content, true);
 
@@ -133,12 +133,12 @@ class Messenger
 
 
     /**
-     * @param string $key
+     * @param  string  $key
      * @return bool
      */
     public function isTesting($key = "APP_ENV")
     {
-        return getenv("testing") === "testing";
+        return getenv($key) === "testing";
     }
 
 
@@ -166,8 +166,8 @@ class Messenger
 
     /**
      * @param $string
-     * @param null $foreground_color
-     * @param null $background_color
+     * @param  null  $foreground_color
+     * @param  null  $background_color
      * @return string
      */
     public function getColoredString($string, $foreground_color = null, $background_color = null)
@@ -176,22 +176,22 @@ class Messenger
 
         // Check if given foreground color found
         if (isset($this->foreground_colors[$foreground_color])) {
-            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+            $colored_string .= "\033[".$this->foreground_colors[$foreground_color]."m";
         }
         // Check if given background color found
         if (isset($this->background_colors[$background_color])) {
-            $colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+            $colored_string .= "\033[".$this->background_colors[$background_color]."m";
         }
 
         // Add string and end coloring
-        $colored_string .= $string . "\033[0m";
+        $colored_string .= $string."\033[0m";
 
         return $colored_string;
     }
 
     /**
-     * @param string $msgType
-     * @param string $colorType
+     * @param  string  $msgType
+     * @param  string  $colorType
      * @return mixed
      */
     private function getColor($msgType = "")
@@ -243,9 +243,9 @@ class Messenger
     // -------------------------------------------------------------------------------------
 
     /**
-     * @param string $type
-     * @param string $msg
-     * @param string $label
+     * @param  string  $type
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function buildMessage($type = "log", $msg = "", $label = "")
@@ -254,7 +254,7 @@ class Messenger
         $label_fg = ($type === "critical") ? "critical" : "black";
 
         if (strlen($label) > 0) {
-            return $this->getColoredString(" " . $label . " ", $label_fg, $color) . " " . $this->getColoredString($msg,
+            return $this->getColoredString(" ".$label." ", $label_fg, $color)." ".$this->getColoredString($msg,
                     $color);
         } else {
             return $this->getColoredString($msg, $color);
@@ -262,169 +262,169 @@ class Messenger
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function log($msg = "", $label = "")
     {
         $msg = $this->buildMessage("log", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function info($msg = "", $label = "")
     {
         $msg = $this->buildMessage("info", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function debug($msg = "", $label = "")
     {
         $msg = $this->buildMessage("debug", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function critical($msg = "", $label = "")
     {
         $msg = $this->buildMessage("critical", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function error($msg = "", $label = "")
     {
         $msg = $this->buildMessage("error", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function success($msg = "", $label = "")
     {
         $msg = $this->buildMessage("success", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function warning($msg = "", $label = "")
     {
         $msg = $this->buildMessage("warning", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function warn($msg = "", $label = "")
     {
         $msg = $this->buildMessage("warn", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function important($msg = "", $label = "")
     {
         $msg = $this->buildMessage("important", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function status($msg = "", $label = "")
     {
         $msg = $this->buildMessage("status", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function notice($msg = "", $label = "")
     {
         $msg = $this->buildMessage("notice", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
 
     /**
-     * @param string $msg
-     * @param string $label
+     * @param  string  $msg
+     * @param  string  $label
      * @return string
      */
     public function note($msg = "", $label = "")
     {
         $msg = $this->buildMessage("warning", $msg, $label);
         if (!$this->isTesting()) {
-            echo $msg . PHP_EOL;
+            echo $msg.PHP_EOL;
         }
         return $msg;
     }
